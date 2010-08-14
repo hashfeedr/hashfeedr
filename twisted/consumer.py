a9fe2e08146cd3de5ae02ae40a437e90332d8256
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-08-14 12:33:59 +0200

    publish incoming tweets synchronously to redis

--- twisted/consumer.py
+++ twisted/consumer.py
@@ -1,9 +1,13 @@
 from twisted.internet import reactor
 import hashdispenser
 import os, sys
+import redis as omg
+
+# connect to redis (synchronous for now)
+redis = omg.Redis()
 
 # omg justin is SOO hot <33333 xD!
 track = ['justin','bieber']
-dispenser = hashdispenser.consume(os.environ['TWUSER'],os.environ['TWPASS'],track)
+dispenser = hashdispenser.consume(os.environ['TWUSER'],os.environ['TWPASS'],redis,track)
 
 reactor.run()
--- twisted/hashdispenser.py
+++ twisted/hashdispenser.py
@@ -1,17 +1,18 @@
-from twisted.internet import reactor
 import TwistedTwitterStream
+from twisted.internet import reactor
 import urllib
 
 class HashDispenser(TwistedTwitterStream.TweetReceiver):
-    def __init__(self):
+    def __init__(self,redis):
+        self.redis = redis
         pass
 
     def tweetReceived(self,tweet):
-        print tweet
+        self.redis.publish('foo',repr(tweet))
         pass
 
-def consume(username,password,track=[]):
-    consumer = HashDispenser()
+def consume(username,password,redis,track=[]):
+    consumer = HashDispenser(redis)
     query = ["track=%s" % ",".join([urllib.quote(s) for s in track])]
     tw = TwistedTwitterStream._TwitterStreamFactory(consumer)
     tw.make_header(username, password, "POST", "/1/statuses/filter.json", "&".join(query))
