6b7a950281149fa487db0ec7cfccb95fec4de9fe
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-08-14 21:19:59 +0200

    major revamp of wsserver.py to use a different websocket lib that does do handshaking

--- wsserver.py
+++ wsserver.py
@@ -1,53 +1,60 @@
 from twisted.internet import reactor, task, protocol, defer
 from twisted.application import service
 from twisted.python import log
-from txwebsockets import WebSocketFactory, BasicOperations
-from txredis.protocol import Redis, RedisSubscriber
-import sys
+from twisted.web import util
+from txredis.protocol import RedisBase, Redis, RedisSubscriber
+import sys, random
+
+# non-pip includes
+sys.path.append("twisted/vendor")
+from txwebsocket import websocket
 
 class HashSubscriber(RedisSubscriber):
     def messageReceived(self, channel, message):
         log.msg("channel %s: message: %s" % (channel, message))
-        self._out("%s: %s" % (channel,message))
+        self.websocket.write("channel %s: message: %s" % (channel, message))
 
-@defer.inlineCallbacks
-def createSubscriber(connection):
-    clientCreator = protocol.ClientCreator(reactor, HashSubscriber)
-    redis = yield clientCreator.connectTCP('localhost', 6379)
-    yield redis.subscribe("special:all")
-    connection.on_subscriber_in_place(redis)
+class HashfeedrWebSocket(websocket.WebSocketHandler):
+    def __init__(self,transport,site):
+        websocket.WebSocketHandler.__init__(self,transport,site)
+        self.connected = True
+        self.createSubscriber()
 
-@defer.inlineCallbacks
-def deleteSubscriber(connection):
-    yield connection.redis.transport.loseConnection()
+    @defer.inlineCallbacks
+    def createSubscriber(self):
+        clientCreator = protocol.ClientCreator(reactor, HashSubscriber)
+        self.redis = yield clientCreator.connectTCP('localhost', 6379)
 
-class HashConnector(BasicOperations):
-    def on_subscriber_in_place(self,redis):
+        # make the redis subscriber aware of the websocket before it
+        # subscribes to the channel
+        self.redis.websocket = self
+        yield self.redis.subscribe("special:all")
+
         # client may already have disconnected before the redis client
         # was connected, so check if we need to destroy it now.
-        if (self.connected):
-            self.redis = redis
-            self.redis._out = self._out
-        else:
-            deleteSubscriber(self)
+        if (not self.connected):
+            self.destroyRedisClient()
 
-    def on_connect(self):
-        self.connected = True
-        createSubscriber(self)
+    @defer.inlineCallbacks
+    def destroyRedisClient(self):
+        yield self.redis.unsubscribe()
+        yield self.redis.transport.loseConnection()
 
-    def after_connection(self):
-        # hack: 1st outgoing frame never reaches the client (weird),
-        # so send an empty frame once the connection is set up.
-        self._out("")
+    def write(self,msg):
+        self.transport.write(msg)
 
-    def on_close(self, r):
-        self.connected = False
-        if (self.redis is not None):
-            deleteSubscriber(self)
-
-    def on_read(self, line):
+    def frameReceived(self,frame):
         pass
 
-factory = WebSocketFactory(HashConnector)
+    def connectionLost(self,reason):
+        log.msg("ws: connection lost")
+        self.connected = False
+        try:
+            self.destroyRedisClient()
+        except exceptions.AttributeError:
+            pass
+
 application = service.Application("hashfeedr")
-reactor.listenTCP(8338, factory)
+site = websocket.WebSocketSite(util.Redirect("http://twitter.com/justinbieber"))
+site.addHandler("/websession", HashfeedrWebSocket)
+reactor.listenTCP(8338, site)
