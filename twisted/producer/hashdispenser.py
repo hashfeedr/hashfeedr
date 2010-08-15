a9030c8bac42382e68954822065b686767b09e3c
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-08-15 17:52:10 +0200

    use chained teardown for lingering connections

--- twisted/producer/hashdispenser.py
+++ twisted/producer/hashdispenser.py
@@ -5,8 +5,7 @@ import os, json, urllib, re
 import photoservices
 
 class HashDispenser(TwistedTwitterStream.TweetReceiver):
-    instance = None
-    firstCallback = None
+    instances = []
 
     @classmethod
     def refresh(klass,terms):
@@ -16,22 +15,35 @@ class HashDispenser(TwistedTwitterStream.TweetReceiver):
         # when there is an instance currently running, we'll have to make sure
         # it disconnects. however, to make sure we don't lose too much tweets,
         # only disconnect it when the first tweet arrives on the new channel
-        if klass.instance is not None:
-            dispenser.firstCallback = klass.instance.disconnect
-        klass.instance = dispenser
+        if len(klass.instances) > 0:
+            dispenser.onConnectionCallback = klass.instances[-1].teardown
+        klass.instances.append(dispenser)
 
         # create a stream for the new dispenser
         usr, pwd = os.environ['TWUSER'], os.environ['TWPASS']
         query = ["track=%s" % ",".join([urllib.quote(s) for s in terms])]
-        tw = TwistedTwitterStream._TwitterStreamFactory(dispenser)
-        tw.make_header(usr, pwd, "POST", "/1/statuses/filter.json", "&".join(query))
-        reactor.connectTCP("stream.twitter.com", 80, tw)
+        factory = TwistedTwitterStream._TwitterStreamFactory(dispenser)
+        factory.make_header(usr, pwd, "POST", "/1/statuses/filter.json", "&".join(query))
+        dispenser.connector = reactor.connectTCP("stream.twitter.com", 80, factory)
+        dispenser.factory = factory
         return dispenser
 
     def __init__(self,terms):
         self.redis = self.__class__.redis
         self.terms = terms
         self.count = 0
+
+    def teardown(self):
+        # remove this connection (which will always be the first element)
+        self.__class__.instances.pop(0)
+
+        # this method can only be called if a new connection is available,
+        # so we're safe to propagate tearing down more connections
+        self.fireCallback()
+
+        # abort everything fo' sho'
+        self.factory.stopTrying()
+        self.connector.disconnect()
 
     def split(self,text):
         sentences = re.split(r"\s*(\.+\s|,+)\s*", text.lower())
@@ -57,12 +69,20 @@ class HashDispenser(TwistedTwitterStream.TweetReceiver):
 
         return json.dumps(composite)
 
-    def tweetReceived(self,tweet):
-        self.count += 1
-        if self.count == 1 and self.firstCallback is not None:
-            log.msg(self, ": firing \"first tweet\" callback")
-            self.firstCallback()
+    # called after a 200 status. we can assume we're registered to receive
+    # all the tracked words after this moment, so can optionally destroy
+    # the previously active stream connection
+    def connectionMade(self):
+        log.msg(self, ": connection made")
+        self.fireCallback()
 
+    def fireCallback(self):
+        if hasattr(self,"onConnectionCallback"):
+            log.msg(self, ": firing onConnectionCallback")
+            self.onConnectionCallback()
+            delattr(self,"onConnectionCallback")
+
+    def tweetReceived(self,tweet):
         terms = self.split(tweet['text'])
         matches = (self.terms & terms)
         if len(matches) > 0:
