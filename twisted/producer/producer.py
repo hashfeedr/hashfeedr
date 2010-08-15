4d764db90daa8dc6cd105ca774ca7bde69c1a209
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-08-15 19:03:23 +0200

    make the producer use txredis

--- twisted/producer/producer.py
+++ twisted/producer/producer.py
@@ -1,16 +1,23 @@
 from twisted.internet import reactor, task, protocol, defer
 from twisted.application import service
 from twisted.python import log
+from txredis.protocol import Redis
 import os, sys
-import redis as omg
+
+# project imports
 from producer import hashdispenser, monitor
 
 # twisted likes this
 application = service.Application("hashfeedr-producer")
 
-# connect to redis (synchronous for now)
-hashdispenser.HashDispenser.redis = omg.Redis()
+@defer.inlineCallbacks
+def start(*args):
+    clientCreator = protocol.ClientCreator(reactor,Redis)
+    redis = yield clientCreator.connectTCP('localhost',6379)
+    hashdispenser.HashDispenser.redis = redis
 
-# make the monitor call class function refresh on the dispenser
-# whenever the set of tracking words has changed
-m = monitor.Monitor(hashdispenser.HashDispenser.refresh)
+    # make the monitor call class function refresh on the dispenser
+    # whenever the set of tracking words has changed
+    m = monitor.Monitor(hashdispenser.HashDispenser.refresh)
+
+start()
