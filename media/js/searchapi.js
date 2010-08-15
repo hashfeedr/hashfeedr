a1d078ba526e32c817ac8cfadf8f1dfaee75d18d
Michiel Heijkoop <mheijkoop@gmail.com>
2010-08-15 21:50:27 +0200

    ignore the empty object from the search api

--- media/js/searchapi.js
+++ media/js/searchapi.js
@@ -3,9 +3,11 @@ function initFromSearchAPI(keyword) {
 		for(var j in data) {
 			var inv = data[j];
 				for(var i in inv) {
-					tweets[tweets.length]=([new Tweet(inv[i].text,inv[i].profile_image_url,inv[i].from_user)]);
-					var t=tweets[tweets.length-1];
-					new Transition(cam.position,cam.position.value,[t[0].position.value[0]+t[0].width*0.5,t[0].position.value[1]+t[0].height*0.5,calcangle()],20.0);
+					if(inv[i].text != undefined) {
+						tweets[tweets.length]=([new Tweet(inv[i].text,inv[i].profile_image_url,inv[i].from_user)]);
+						var t=tweets[tweets.length-1];
+						new Transition(cam.position,cam.position.value,[t[0].position.value[0]+t[0].width*0.5,t[0].position.value[1]+t[0].height*0.5,calcangle()],20.0);
+					}
 				}
 			}	
 	});
