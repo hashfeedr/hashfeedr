6adf9daedf8b9880d586957d5b8912d1fb5b120a
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-08-15 11:38:31 +0200

    make dispenser aware of monitor

--- twisted/producer/hashdispenser.py
+++ twisted/producer/hashdispenser.py
@@ -1,16 +1,18 @@
 import TwistedTwitterStream
 from twisted.internet import reactor
 from twisted.python import log
-import json, urllib, re
+import os, json, urllib, re
 
 class HashDispenser(TwistedTwitterStream.TweetReceiver):
     @classmethod
-    def consume(klass,username,password,redis,track=[]):
+    def consume(klass,monitor,redis,track=[]):
         consumer = klass(redis)
+        usr, pwd = os.environ['TWUSER'], os.environ['TWPASS']
         query = ["track=%s" % ",".join([urllib.quote(s) for s in track])]
         tw = TwistedTwitterStream._TwitterStreamFactory(consumer)
-        tw.make_header(username, password, "POST", "/1/statuses/filter.json", "&".join(query))
+        tw.make_header(usr, pwd, "POST", "/1/statuses/filter.json", "&".join(query))
         reactor.connectTCP("stream.twitter.com", 80, tw)
+        consumer.monitor = monitor
         return consumer
 
     def __init__(self,redis):
