abc2ff4216bf468de1ee07f04d205b28db03b4cd
Emil Loer <emil@koffietijd.net>
2010-08-29 12:22:58 +0200

    rewrote the clientside feeder to use css3 transforms

--- media/js/feedr.js
+++ media/js/feedr.js
@@ -16,8 +16,8 @@ var placementx=100.0;
 var campos=[0,0];
 var camrot=0.0;
 
-//var backdrop=new Image();
-//backdrop.src="/media/images/feedbackdrop.jpg";
+// new
+var vp,scene;
 
 function catmullrom(t,x0,x1,x2,x3){
 	var t2=t*t,t3=t2*t;
@@ -29,8 +29,10 @@ function lerp(t,a,b) {
 }
 
 function placement(x) {
-	var t=x*0.006;
-	return (Math.sin(t)*2.0+Math.sin(2.232*t)+Math.sin(9.876*t)*5.0+Math.sin(18.888*t))*50.0;
+//	var t=x*0.006;
+//	return (Math.sin(t)*2.0+Math.sin(2.232*t)+Math.sin(9.876*t)*5.0+Math.sin(18.888*t))*50.0;
+	var t=x*0.002;
+	return (Math.sin(t)*2.0+Math.sin(2.232*t)+Math.sin(9.876*t)*5.0+Math.sin(18.888*t))*40.0;
 }
 
 function calcangle() {
@@ -45,17 +47,21 @@ function boxintersect(x1,y1,w1,h1,x2,y2,w2,h2) {
           y2 <= y1+h1);
 }
 
-function eradicate() {
-	for (var i=0;i<tweets.length;++i) {
-		if (tweets[i][0].position.value[0]<placementx-2000*aspect) {
-			tweets[i][0].release();
-			delete tweets[i][0];
-			tweets.splice(i--,1);
+function breaktext(str, linelength) {
+	var l=str.split(" ");
+	var s="",n=0;
+	for (var i=0;i<l.length;++i) {
+		s+=l[i]+" ";
+		n+=l[i].length;
+		if (n>linelength) {
+			s+="<br/>";
+			n=0;
 		}
 	}
+	return s;
 }
 
-function findnewpos(tw) {
+function findnewpos_old(tw) {
 	var found=false,x=0,y=0;
 	var w=tw.width;
 	var h=tw.height;
@@ -74,34 +80,40 @@ function findnewpos(tw) {
 	tw.position.value=[x,y];
 }
 
-function textmetrics(fsize,s) {
-	var lines=s.split("\n");
-	var w=0,h=0;
-	for (var i in lines) {
-		w=Math.max(c.measureText(lines[i]).width,w);
-		h+=fsize;
+function findnewpos(w,h) {
+	var found=false,x=0,y=0;
+	while (!found) {
+		found=true;
+		placementx+=1.0;
+		x=placementx-w*0.5;
+		y=placement(placementx)-h*0.5;
+		for (var i=0;i<tweets.length;++i) {
+			if (boxintersect(x,y,w,h,tweets[i].x,tweets[i].y,tweets[i].width,tweets[i].height)) {
+				found=false;
+				break;
+			}
+		}
 	}
-	return [w,h];
-}
-
-function filltext(fsize,s,x,y) {
-	var lines=s.split("\n");
-	var h=0;
-	for (var i in lines) {
-		c.fillText(lines[i],x,y+h);
-		h+=fsize;
-	}
+	return [x,y];
 }
 
 /** @constructor */
 function Camera() {
-	this.position=new Property([0.0,0.0,0.0]); // x,y,angle
-	
-	this.activate=function() {
-		c.translate(800*aspect,500);
-		c.rotate(this.position.value[2]);
-		c.translate(-this.position.value[0],-this.position.value[1]);
-	}
+	this.x=0;
+	this.y=0;
+	this.angle=0;
+
+	// TODO: uniform scaling
+
+	this.activate = function() {
+		scene.css("-webkit-transform","rotate("+this.angle+"deg) translate3d("+((width*0.5)-this.x)+"px,"+((height*0.5)-this.y)+"px,0)");
+	};
+
+	this.move = function(x,y,angle) {
+		this.x=x;
+		this.y=y;
+		this.angle=angle;
+	};
 }
 var cam=new Camera();
 
@@ -110,211 +122,38 @@ function easeinout(t) {
 	return -0.5*(Math.cos(Math.PI*t)-1);
 }
 
-/** @constructor */
-function Property(value) {
-	this.length=value.length;
-	this.value=value;
-}
-
-/** @constructor */
-function Transition(property,start,end,duration) {
-	this.start=start;
-	this.end=end;
-	this.property=property;
-	this.time=0.0;
-	this.step=1.0/duration;
-	transitions.push(this);
-
-	this.update=function() {
-		this.time+=this.step;
-		if (this.time>1.0) {
-			this.property.value=this.end;
-			return false;
-		}
-		for (var i=0;i<this.property.length;++i) {
-			this.property.value[i]=lerp(easeinout(this.time),this.start[i],this.end[i]);
-		}
-		return true;
-	}
-}
-
-/** @constructor */
-function PictureBox(src,tweetpos) {
-	this.position=new Property([0,0]);
-	this.image=new Image();
-	this.image.src=src;
-	this.loaded=false;
-	this.width=0;
-	this.height=0;
-	this.margin=16;
-	this.padding=8;
-	this.tweetpos=tweetpos;
-
-	var self=this;
-	this.image.onload=function() {
-		self.loaded=true;
-		self.width=this.width+self.margin*2+self.padding*2;
-		self.height=this.height+self.margin*2+self.padding*2;
-		findnewpos(self);
-		tweets[tweets.length]=[self];
-		scene[scene.length]=[new TweetLine([self.position.value[0]+self.width*0.5,self.position.value[1]+self.width*0.5],self.tweetpos)];
-	};
-
-	this.release=function() {
-		delete this.image;
-		delete this.position;
-	};
-
-	this.draw=function() {
-		if (self.loaded) {
-			c.save();
-			c.translate(this.position.value[0],this.position.value[1]);
-			c.fillStyle="rgba(0,0,0,1.0)";
-			c.fillRect(this.margin,this.margin,this.width-this.margin*2,this.height-this.margin*2);
-			c.strokeStyle="rgba(255,255,255,0.6)";
-			c.strokeRect(this.margin,this.margin,this.width-this.margin*2,this.height-this.margin*2);
-			c.drawImage(this.image,this.margin+this.padding,this.margin+this.padding);
-			c.restore();
-		}
-	};
-}
-
-/** @constructor */
-function Tweet(message,avatar,username) {
-	this.position=new Property([100,10]);
-	this.charpos=new Property([0]);
-	this.margin=16;
-	this.padding=8;
-	this.message=message;
-	this.username=username;
-	
-	var l=this.message.split(" ");
-	var s="",n=0;
-	for (var i=0;i<l.length;++i) {
-		s+=l[i]+" ";
-		n+=l[i].length;
-		if (n>linelength) {
-			s+="\n";
-			n=0;
-		}
-	}
-	this.message=s;
-
-	c.font=fontsize+"px "+fontface;
-	var exts=textmetrics(fontsize,this.message);
-	this.width=64+this.padding*3+exts[0]+2*this.margin;
-	this.height=Math.max(exts[1],64)+2*this.padding+2*this.margin+fontsize;
-	findnewpos(this);
-
-	this.avatar=new Image();
-	this.avatar.src=avatar.replace(/normal.jpg$/,"bigger.jpg");
-
-	new Transition(this.charpos,[0],[this.message.length],10);
-	
-	this.release=function() {
-		delete this.avatar;
-		delete this.position;
-		delete this.charpos;
-	};
-	
-	this.draw=function() {
-		c.shadowColor="rgba(0,0,0,0.0)";
-		c.shadowOffsetX=0;
-		c.shadowOffsetY=0;
-		c.shadowBlur=16;
-
-		c.save();
-		c.translate(this.position.value[0],this.position.value[1]);
-		
-		c.fillStyle="#040a14";
-		c.fillRect(this.margin,this.margin,this.width-this.margin*2,this.height-this.margin*2);
-		c.strokeStyle="rgba(255,255,255,0.8)";
-		c.strokeRect(this.margin,this.margin,this.width-this.margin*2,this.height-this.margin*2);
-		
-		if (this.avatar.complete==true) {
-			c.shadowBlur=0;
-			try {
-				c.drawImage(this.avatar,this.margin+this.padding,this.margin+this.padding,64,64);
-			} catch(err) {
-				// fail silently :)
-			}
-		}
-
-		c.fillStyle="#79a631";
-		c.textAlign="right";
-		c.textBaseline="bottom";
-		c.font=fontsize+"px DroidSansBold";
-		c.fillText(this.username,this.width-this.margin-this.padding*2,this.height-this.margin-this.padding);
-		c.fillStyle="#fff";
-		c.textBaseline="top";
-		c.textAlign="left";
-		c.shadowColor="rgba(0,0,0,0.0)";
-		c.shadowOffsetX=2;
-		c.shadowOffsetY=2;
-		c.shadowBlur=4;
-		c.font=fontsize+"px "+fontface;
-		filltext(fontsize,this.message.substr(0,this.charpos.value[0]),this.margin+this.padding*2+64,this.margin+this.padding-4);
-		c.restore();
-	};
-}
-
-/** @constructor */
-function GrowArrow(p0,p1,p2,p3) {
-	this.p0=new Property(p0);
-	this.p1=new Property(p1);
-	this.p2=new Property(p2);
-	this.p3=new Property(p3);
-	this.steps=new Property([0]);
-	new Transition(this.steps,[0],[25],20);
-	this.maxsteps=25.0;
-	this.increment=1.0/this.maxsteps;
-
-	this.draw=function() {
-		c.strokeStyle="#000";
-		c.lineWidth=10;
-		c.shadowBlur=40;
-		c.shadowColor="#f00";
-		c.beginPath();
-		for (var i=0;i<this.steps.value[0];++i) {
-			var t=i*this.increment;
-			c.lineTo(catmullrom(t,this.p0.value[0],this.p1.value[0],this.p2.value[0],this.p3.value[0]),
-			         catmullrom(t,this.p0.value[1],this.p1.value[1],this.p2.value[1],this.p3.value[1]));
-		}
-		c.stroke();
-	}
-}
-
-/** @constructor */
-function TweetLine(p0,p1) {
-	this.p0=new Property([p0[0],p0[1]]);
-	this.p1=new Property([p1[0],p1[1]]);
-	new Transition(this.p0,[p1[0],p1[1]],[p0[0],p0[1]],20);
-
-	scene[scene.length]=[this];
-
-	this.draw=function() {
-		c.strokeStyle="#ccc";
-		c.beginPath();
-		c.lineWidth=10;
-		c.moveTo(this.p0.value[0],this.p0.value[1]);
-		c.lineTo(this.p1.value[0],this.p1.value[1]);
-		c.stroke();
-	};
-}
-
-//scene[scene.length]=[new TweetLine([0,0],[200,200])];
-
-function resizecanvas() {
-	width=c.canvas.clientWidth;
-	height=c.canvas.clientHeight;
-	c.canvas.width=width;
-	c.canvas.height=height;
-	c.textBaseline="top";
+function resizescene() {
+	width=$("#viewport").width();
+	height=$("#viewport").height();
 
 	scalefactor=parseFloat(height)/1000.0;
-	aspect=parseFloat(width)/parseFloat(height)
+	aspect=parseFloat(width)/parseFloat(height);
 
-	draw();
+	// TODO: reapply camera
+}
+
+function addtweet(txt,image,screenname) {
+//	var txt="'Tulp' het hoogtepunt van mijn #nzon10? Kris en Lex Vesseur hebben het voor de neus van Mads weten weg te pikken. Een 9. #Formatgava.";
+//	txt=txt.slice(0,Math.random()*txt.length);
+
+	txt=breaktext(txt,25);
+	image=image.replace(/normal(\.\w{3})$/,"bigger$1");
+	addobj("tweet","<img src=\""+image+"\"/><p>"+txt+"</p><p class=\"username\">"+screenname+"</p>");
+}
+
+function addobj(cls, html) {
+	var count=tweets.length+1;
+	scene.append("<div class=\""+cls+"\" id=\"obj"+count+"\">"+html+"</div>");
+	var t=$("#obj"+count);
+	var w=t.outerWidth(); // This also forces a reflow of the layout, applying the initial css so that the transition occurs
+	var h=t.outerHeight();
+	var pos=findnewpos(w+16,h+16);
+	t.css({"-webkit-transform":"translate("+pos[0]+"px,"+pos[1]+"px)","-webkit-transitxion":"all .8s ease-out"});
+	t.css({"visibility":"visible"});
+	cam.move(pos[0]+w*0.5,pos[1]+h*0.5,Math.random()*20.0-10.0);
+	cam.activate();
+	tweets.push({x:pos[0],y:pos[1],width:w+16,height:h+16,id:"#obj"+count});
+	//t.css({"-webkit-transform":"rotate(30deg)"});
 }
 
 function update() {
@@ -331,53 +170,30 @@ function update() {
 	}
 }
 
-function draw() {
-//	if (backdrop.complete==true) {
-//		c.drawImage(backdrop,0,0,width,height);
-//	}
-	c.fillStyle="#0b1d3a";
-	c.fillRect(0,0,width,height);
-	c.save();
-	c.scale(scalefactor,scalefactor);
-	cam.activate();
-//	c.strokeStyle="#fff";
-//	c.beginPath();
-//	for (var i=0;i<1000.0;i+=1) {
-//		c.lineTo(i,placement(i));
-//	}
-//	c.stroke();
-	for (var e=0;e<scene.length;e++) {
-		c.save();
-		scene[e][0].draw();
-		c.restore();
-	}
-	for (var e=0;e<tweets.length;e++) {
-		c.save();
-		tweets[e][0].draw();
-		c.restore();
-	}
-	c.restore();
-
-	c.fillText("#tweets: "+tweets.length,0,0);
-}
-
 function parsemessage(msg) {
 	var data=$.parseJSON(msg);
-	tweets[tweets.length]=([new Tweet(data.tweet.text,data.tweet.user.profile_image_url,data.tweet.user.screen_name)]);
-	var t=tweets[tweets.length-1];
-	new Transition(cam.position,cam.position.value,[t[0].position.value[0]+t[0].width*0.5,t[0].position.value[1]+t[0].height*0.5,calcangle()],20.0);
-	if ("image" in data) {
-		new PictureBox(data.image,[t[0].position.value[0]+t[0].width*0.5,t[0].position.value[1]+t[0].height*0.5]);
-	}
+//	tweets[tweets.length]=([new Tweet(data.tweet.text,data.tweet.user.profile_image_url,data.tweet.user.screen_name)]);
+//	var t=tweets[tweets.length-1];
+//	new Transition(cam.position,cam.position.value,[t[0].position.value[0]+t[0].width*0.5,t[0].position.value[1]+t[0].height*0.5,calcangle()],20.0);
+//	if ("image" in data) {
+//		new PictureBox(data.image,[t[0].position.value[0]+t[0].width*0.5,t[0].position.value[1]+t[0].height*0.5]);
+//	}
+	addtweet(data.tweet.text,data.tweet.user.profile_image_url,data.tweet.user.screen_name);
 }
 
 $(function(){
-	c=$("#thecanvas").get(0).getContext("2d");
+	vp=$("#viewport");
+	scene=$("#scene");
 
-	resizecanvas();
-	$(window).resize(resizecanvas);
+	resizescene();
+	$(window).resize(resizescene);
+	scene.css("-webkit-transform","translate3d("+(width*0.5)+"px,"+(height*0.5)+"px,0)");
 
-	$("#thecanvas").click(function(){
+	vp.click(function(){
+		addtweet();
+		//cam.move(200,200,Math.random()*40.0-20.0);
+//		cam.move(200,200,0);
+//		cam.activate();
 //		var t=new Tweet("RT justin bieber was a freaking lolcat abuser, he totally biebered them ololololol bla bla blah! 12345");
 //		scene.push(t);
 //		new Transition(cam.position,cam.position.value,[t.position.value[0]+t.width*0.5,t.position.value[1]+t.height*0.5,calcangle()],20.0);
@@ -387,7 +203,7 @@ $(function(){
 //	new Transition(cam.position,[0.0,20.0,0.0],[200.0,0.0,1.0],100.0);
 
 	setInterval(function(){
-		if (update()) draw();
+//		if (update()) draw();
 	},framedelay);
       
 	var ws = new WebSocket(websocket_url);
